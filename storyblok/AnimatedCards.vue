43b6ab2b4a6368f2c326100fdf9f625c2a2ee725
Guillem <guillem@disedit.com>
2025-02-03 16:18:35 +0100

    preload animated card images

--- storyblok/AnimatedCards.vue
+++ storyblok/AnimatedCards.vue
@@ -18,12 +18,44 @@ onMounted(() => {
 onUnmounted(() => {
   clearInterval(interval)
 })
+
+/*
+// Preload images
+const img = useImage()
+const images = computed(() => {
+  let preloadImages = []
+  props.blok.cards.forEach(card => {
+    const mediaCards = card.columns.filter(block => block.component === 'Media')
+    mediaCards.forEach(mediaCard => {
+      const url = img(mediaCard.media.filename, { width: 800 })
+      preloadImages.push(url)
+    })
+  })
+  return preloadImages.map(href => ({ rel: "prefetch", href }))
+})
+
+useHead({
+  link: images.value
+})
+*/
 </script>
 
 <template>
   <section v-editable="blok">
     <template v-for="(card, i) in blok.cards" :key="card._uid">
-      <StoryblokComponent v-if="i === currentCard" :blok="card"/>
+      <StoryblokComponent v-if="i === currentCard" :blok="card" />
     </template>
   </section>
+  <div class="preload sr-only">
+    <template v-for="card in blok.cards" :key="card._uid">
+      <template v-for="column in card.columns" :key="column._uid">
+        <NuxtImg
+          v-if="column.component === 'Media'"
+          :src="column.media.filename"
+          :width="800"
+          preload
+        />
+      </template>
+    </template>
+  </div>
 </template>
