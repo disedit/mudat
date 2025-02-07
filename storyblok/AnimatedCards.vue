9ef787adc0e8c7629294bf7e22d5e5ef743aedb3
Guillem <guillem@disedit.com>
2025-02-07 16:55:27 +0100

    Update AnimatedCards.vue

--- storyblok/AnimatedCards.vue
+++ storyblok/AnimatedCards.vue
@@ -1,26 +1,9 @@
 <script setup>
 const props = defineProps({ blok: Object })
 
-const preferredMotion = usePreferredReducedMotion()
-const currentCard = ref(0)
-const totalCards = computed(() => props.blok.cards.length)
-let interval
 const loaded = reactive({})
 
 onMounted(() => {
-  const baseDuration = props.blok.interval || 2000
-  const duration = (preferredMotion.value === 'reduce') ? baseDuration * 2 : baseDuration
-
-  interval = setInterval(() => {
-    if (!props.blok.loop && currentCard.value >= totalCards.value - 1) {
-      clearInterval(interval)
-      interval = null
-      return
-    }
-
-    currentCard.value = (currentCard.value >= totalCards.value - 1) ? 0 : currentCard.value + 1
-  }, duration)
-
   props.blok.cards.forEach(card => {
     card.columns.forEach(column => {
       if (column.component === 'AnimatedMedia') {
@@ -36,10 +19,6 @@ onMounted(() => {
   })
 })
 
-onUnmounted(() => {
-  clearInterval(interval)
-})
-
 const allImagesLoaded = computed(() => {
   return Object.values(loaded).every(value => !!value)
 })
@@ -50,11 +29,8 @@ function setLoaded (id) {
 </script>
 
 <template>
-  <section v-editable="blok" v-if="allImagesLoaded" class="h-viewport">
+  <section v-editable="blok" class="h-viewport">
     <UtilsBurstCards :cards="blok.cards" v-if="allImagesLoaded" />
-    <template v-for="(card, i) in blok.cards" :key="card._uid">
-      <StoryblokComponent v-if="i === currentCard" :blok="card" />
-    </template>
   </section>
   <div class="preload sr-only" aria-hidden="true" v-if="Object.values(loaded).length > 0">
     <template v-for="card in blok.cards" :key="card._uid">
